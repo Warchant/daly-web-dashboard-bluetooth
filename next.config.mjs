37b6fb3c1761768d4ed2fcde1fecd0e38f306197
Bohdan Vanieiev <warchantua@gmail.com>
2024-05-26 23:05:33 +0300

    Use PWA

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,15 @@ const nextConfig = {
     output: 'export',
 };
 
-export default nextConfig;
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  reloadOnOnline: true,
+});
+
+export default withSerwist(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { BluetoothProvider } from "./bt_provider";
@@ -6,10 +6,44 @@ import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const TITLE = "Daly BMS Dashboard";
+const TEMPLATE = "%s - Dashboard";
+const DESCRIPTION = "Dashboard which is automatically updated with the latest data from a Daly BMS over Bluetooth.";
+
 export const metadata: Metadata = {
-	title: "Daly BMS Dashboard",
-	description:
-		"Dashboard which is automatically updated with the latest data from a Daly BMS over Bluetooth.",
+	applicationName: TITLE,
+	manifest: "/manifest.json",
+	title: TITLE,
+	authors: [{ name: "Bohdan Vanieiev", url: "https://github.com/warchant" }],
+	creator: "Bohdan Vanieiev",
+	icons: [
+		{ rel: "icon", sizes: "16x16", url: "/favicon-16x16.png" },
+		{ rel: "icon", sizes: "32x32", url: "/favicon-32x32.png" },
+		{ rel: "apple-touch-icon", url: "apple-touch-icon.png" }
+	],
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: "default",
+		title: TITLE,
+	},
+	formatDetection: {
+		telephone: false
+	},
+	openGraph: {
+		type: "website",
+		siteName: TITLE,
+		description: DESCRIPTION,
+		title: {
+			default: TITLE,
+			template: TEMPLATE,
+		}
+	},
+	description: DESCRIPTION,
+};
+
+
+export const viewport: Viewport = {
+	themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
